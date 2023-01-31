bce087e2920281b3cb41c064b5e05cdda0a754b8
Nicolás Rodrigo Díaz <nicolas.diaz.92@gmail.com>
2023-01-31 12:48:37 +0800

    index

--- cmd/kube-plex/main_windows.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "os"
-
-// Only signal to catch in windows is os.Interrupt
-var shutdownSignals = []os.Signal{os.Interrupt}
-
-// protectSigKill is a no-op on Windows
-func protectSigKill(ctx context.Context) context.Context { return ctx }
